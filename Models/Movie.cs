d1cfbfa2775f9cdf582319759dbf08fdc8682b79
Joao Secundo <alundra@academico.ufs.br>
2022-07-17 19:25:05 -0300

    Movie model

---
+++ Models/Movie.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MoviesAPI.Models
+{
+    public class Movie
+    {
+        public string Title {get; set;}
+        public string Genre {get; set;}
+        public string Director {get; set;}
+        public string Duration {get; set;}
+    }
+}
